990d629c6e9e250ab867cb37327b6ae735378d32
Matteo Orzes <orzesmatteo1993@gmail.com>
2023-09-10 10:54:34 +0200

    Added dto and mapper stub

--- challenge-server/src/test/java/it/milkman/chellenge/server/ChallengeServerApplicationTests.java
+++ challenge-server/src/test/java/it/milkman/chellenge/server/ChallengeServerApplicationTests.java
@@ -1,13 +1,14 @@
 package it.milkman.chellenge.server;
 
+import it.milkman.challenge.server.ChallengeApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ChallengeApplication.class)
 class ChallengeServerApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+    @Test
+    void contextLoads() {
+    }
 
 }
